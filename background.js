b8dc11efac665f412d0c3f0361727a1f65c7c9b9
shaki <developerhubytchannel@gmail.com>
2023-12-27 03:42:00 +0600

    link updated

---
+++ background.js
@@ -0,0 +1,55 @@
+chrome.contextMenus.create({
+  id: "defaultTheme",
+  title: "Default Theme",
+  contexts: ["all"],
+});
+chrome.contextMenus.create({
+  id: "theme01",
+  title: "Theme 01",
+  contexts: ["all"],
+});
+chrome.contextMenus.create({
+  id: "theme02",
+  title: "Theme 02",
+  contexts: ["all"],
+});
+chrome.contextMenus.create({
+  id: "theme03",
+  title: "Theme 03",
+  contexts: ["all"],
+});
+chrome.contextMenus.create({
+  id: "theme04",
+  title: "Theme 04",
+  contexts: ["all"],
+});
+chrome.contextMenus.create({
+  id: "theme05",
+  title: "Theme 05",
+  contexts: ["all"],
+});
+chrome.contextMenus.create({
+  id: "theme06",
+  title: "Theme 06",
+  contexts: ["all"],
+});
+chrome.contextMenus.create({
+  id: "theme07",
+  title: "Theme 07",
+  contexts: ["all"],
+});
+chrome.contextMenus.create({
+  id: "theme08",
+  title: "Theme 08",
+  contexts: ["all"],
+});
+chrome.contextMenus.create({
+  id: "theme09",
+  title: "Theme 09",
+  contexts: ["all"],
+});
+chrome.contextMenus.create({
+  id: "theme10",
+  title: "Theme 10",
+  contexts: ["all"],
+});
